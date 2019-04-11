f2b9177b5c22e5a2965e0947d8b7f7b76414f9d0
Elizabeth Phillips <Elizabeth.Phillips.Contractor@dvla.gov.uk>
2019-04-11 14:46:01 +0100

    update POM

---
+++ src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,12 @@
+import org.springbootframework.boot.autoconfigure;
+
+package io.javabrains.springbootstarter;
+
+
+@SpringBootApplication
+public class CourseApiApp {
+
+    public static void main(String[] args) {
+
+    }
+}
