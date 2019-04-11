880b1b844019ff3d35fe59b30f01577f2740a5f6
Elizabeth Phillips <Elizabeth.Phillips.Contractor@dvla.gov.uk>
2019-04-11 15:50:59 +0100

    added getters and setters

---
+++ src/main/java/io/javabrains/springbootstarter/topic/Topic.java
@@ -0,0 +1,15 @@
+package io.javabrains.springbootstarter.topic;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Topic {
+
+    private String id;
+    private String name;
+    private String description;
+}
