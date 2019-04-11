d3a5012eccca679be9180dcce1286a419a11a92d
Elizabeth Phillips <Elizabeth.Phillips.Contractor@dvla.gov.uk>
2019-04-11 15:22:01 +0100

    update ourseApiApp

--- src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
+++ src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseApiApp {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(CourseApiApp.class, args);
     }
 }
